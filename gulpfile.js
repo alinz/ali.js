12aa7f94029cc7471c7f7389f011398c4120941e
Ali Najafizadeh <a.najafizadeh@gmail.com>
2014-12-17 23:17:47 -0500

    added proper error handling

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,33 @@
-var gulp        = require("gulp"),
-    del         = require('del'),
-    browserify  = require('browserify'),
-    watchify    = require('watchify'),
-    source      = require('vinyl-source-stream'),
-    buffer      = require('vinyl-buffer'),
-    reactify    = require('reactify'),
-    uglify      = require('gulp-uglify'),
-    browserSync = require('browser-sync'),
-    reload      = browserSync.reload;
+var gulp          = require("gulp"),
+    del           = require('del'),
+    Notification  = require('node-notifier'),
+    util          = require('gulp-util'),
+    browserify    = require('browserify'),
+    watchify      = require('watchify'),
+    source        = require('vinyl-source-stream'),
+    buffer        = require('vinyl-buffer'),
+    reactify      = require('reactify'),
+    uglify        = require('gulp-uglify'),
+    browserSync   = require('browser-sync'),
+    reload        = browserSync.reload;
 
 var path = {
     jsx:    "./src/main.jsx",
     bundle: "ali.js",
     distjs: "dist/js"
 };
+
+function standardHandler(err){
+  // Notification
+  Notification.notify({ message: 'Error: ' + err.message });
+  // Log to console
+  util.log(util.colors.red('Error'), err.message);
+}
+
+function browserifyHandler(err){
+  standardHandler(err);
+  this.end();
+}
 
 //delete dist folder by calling: gulp clean
 gulp.task('clean', function(cb) {
@@ -25,21 +39,29 @@ gulp.task('browserSync', function() {
     server: {
       baseDir: './'
     }
-  })
+  });
 });
 
 gulp.task('watchify', function() {
   var bundler = watchify(browserify(path.jsx, watchify.args));
 
+  bundler.on("error", function (err) { console.log(err); });
+
   function rebundle() {
     return bundler
             .bundle()
+            .on('error', standardHandler)
             .pipe(source(path.bundle))
+            .on('error', standardHandler)
             .pipe(gulp.dest(path.distjs))
-            .pipe(reload({ stream: true }));
+            .on('error', standardHandler)
+            .pipe(reload({ stream: true }))
+            .on('error', standardHandler);
   }
 
-  bundler.transform(reactify).on("update", rebundle);
+  bundler.transform(reactify)
+    .on("update", rebundle)
+    .on('error', standardHandler);
 
   return rebundle();
 });
