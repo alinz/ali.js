34d242315b24d58e81e84a0b686b55727ff06978
Ali Najafizadeh <a.najafizadeh@gmail.com>
2014-12-19 11:14:21 -0500

    added license

--- src/js/mixins/event-emitter.js
+++ src/js/mixins/event-emitter.js
@@ -1,3 +1,12 @@
+/**
+* Copyright 2014, Ali Najafizadeh.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree.
+*
+*/
+
 "use strict";
 
 module.exports = {
